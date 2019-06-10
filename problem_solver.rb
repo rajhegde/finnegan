094f5c9820f640a9b87d01bceb6f674db0216d55
rajhegde <44220868+rajhegde@users.noreply.github.com>
2019-06-10 02:56:17 -0400

    added more methods

--- problem_solver.rb
+++ problem_solver.rb
@@ -7,4 +7,20 @@ class ProblemSolver
 
     def solve_tough_problem
     end
+
+    def solve_climb_problem
+        hill_climb = HillClimb.new
+        hill_climb.climb
+    end
+
+    def use_genetic_algorithm(problem)
+        darwin = Darwin.new(problem)
+        darwin.survive
+    end
+
+    def prolog_solver(description, question)
+        prolog = Prolog.new
+        prolog.describe_world(description)
+        prolog.ask(question)
+    end
 end
